81e89e274aaada20673d5c9ac110922822bb9513
SasoOO96 <95930983+SasoOO96@users.noreply.github.com>
2025-02-24 01:37:38 +0100

    First Commit
    First Date: 24-02-2025

---
+++ MeteoroloskaStanica/Migrations/20250222110445_V1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Met_Stanica.Migrations
+{
+    /// <inheritdoc />
+    public partial class V1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Gradovi",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ime = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    X = table.Column<double>(type: "float", nullable: false),
+                    Y = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gradovi", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Podaci",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Mesec = table.Column<int>(type: "int", nullable: false),
+                    PTemperatura = table.Column<int>(type: "int", nullable: false),
+                    KolicinaPadavina = table.Column<int>(type: "int", nullable: false),
+                    BrSDana = table.Column<int>(type: "int", nullable: false),
+                    GradID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Podaci", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Podaci_Gradovi_GradID",
+                        column: x => x.GradID,
+                        principalTable: "Gradovi",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Podaci_GradID",
+                table: "Podaci",
+                column: "GradID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Podaci");
+
+            migrationBuilder.DropTable(
+                name: "Gradovi");
+        }
+    }
+}
